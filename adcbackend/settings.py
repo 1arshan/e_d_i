7e52a333c2569e9082dd3064118059bf6730111d
1arshan <1arshanahmad@gmail.com>
2020-07-04 16:04:02 +0530

    preparing for deployment1

--- adcbackend/settings.py
+++ adcbackend/settings.py
@@ -113,11 +113,11 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ],
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ]
 }
 
 # Internationalization
