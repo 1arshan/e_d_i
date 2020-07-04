4d6568eea7cc08bda63ebeb54900df7fe69ccd11
1arshan <1arshanahmad@gmail.com>
2020-07-05 01:28:06 +0530

    preparing for deploymen14

--- adcbackend/settings.py
+++ adcbackend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = ProjectSecretKey.project_secret
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
