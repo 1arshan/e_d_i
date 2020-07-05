84d354a1c1f58ec3738c3ceefb4039af400db8fa
1arshan <1arshanahmad@gmail.com>
2020-07-05 13:33:18 +0530

    preparing for deploymen33

--- user_login/models.py
+++ user_login/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from subject_material.models import VideoMaterial
+
+
+class DoubtsQuestion(models.Model):
+    material_link =models.ForeignKey(VideoMaterial,on_delete=models.CASCADE)
+    doubts_question =models.TextField()
+
+class DoubtsQuestionPhotos(models.Model):
+    doubts_question_link =models.ForeignKey(DoubtsQuestion,on_delete=models.CASCADE)
 
 
