de0bccbf024493e04ff2723bb30beae7e3b0dc7c
1arshan <1arshanahmad@gmail.com>
2020-07-24 21:11:18 +0530

    teacher subject update from admin panel

--- user_signup/admin.py
+++ user_signup/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import TempStudent, StudentProfile, TempTeacher, TeacherProfile
-
+from django_better_admin_arrayfield.admin.mixins import DynamicArrayMixin
 
 @admin.register(TempStudent)
 class TempStudentAdmin(admin.ModelAdmin):
@@ -22,5 +22,5 @@ class StudentProfileInline(admin.ModelAdmin):
 
 
 @admin.register(TeacherProfile)
-class TeacherProfileInline(admin.ModelAdmin):
+class TeacherProfileInline(admin.ModelAdmin,DynamicArrayMixin):
     list_display = ("phone_number", "email", 'first_name', 'last_name')
