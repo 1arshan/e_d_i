aca0a73dd2e4c4babda7d7c15d4d5cf0007c0171
Ubuntu <ubuntu@ip-172-31-15-145.ap-south-1.compute.internal>
2020-07-25 20:17:18 +0000

    removing bug in s3 deployment

--- adcbackend/settings.py
+++ adcbackend/settings.py
@@ -160,11 +160,11 @@ AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
-AWS_LOCATION = 'production'
+AWS_LOCATION = 'static-production'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'adcbackend/static'),
 ]
 STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'adcbackend.storage_backends.MediaStorage'
